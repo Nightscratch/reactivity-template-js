e9b47bc2b824eebe2fc626bdfc6d713c2744d1ed
Nightscratch <1705755479@qq.com>
2023-01-01 22:57:43 +0800

    del

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,3 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
@@ -10,8 +8,6 @@ export default {
     format: 'es',
   },
   plugins: [
-    resolve(),
-    commonjs(),
     terser()
   ]
 };
